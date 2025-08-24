5c978f67d38df9dc7f82ed992ba9ba513126fb4c
edilmazhitov <zusupovaerkinai@mail.com>
2025-08-24 13:30:13 +0600

    rebirth price update

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ class Combat {
     rebirthCount: '[data-js-rebirth-count]',
     rebirthBtn: '[data-js-rebirth-btn]',
     rebirthPrice: '[data-js-rebirth-price]',
+    rebirthClick: '[data-js-click]'
   }
 
   constructor() {
@@ -15,6 +16,7 @@ class Combat {
 
     this.rebirth = Number(localStorage.getItem('rebirth')) || 0
     this.rebirthPrice = Number(localStorage.getItem('rebirthPrice')) || 100
+    this.rebirthAddPlusMust = Number(localStorage.getItem("rebirthAddPlusMust")) || 10
 
     this.balance = document.querySelector(this.selectors.balance)
     this.plusBtn = document.querySelector(this.selectors.plus)
@@ -24,6 +26,7 @@ class Combat {
 
     this.rebirthCount = document.querySelector(this.selectors.rebirthCount)
     this.rebirthBtn = document.querySelector(this.selectors.rebirthBtn)
+    this.rebirthClickEl = document.querySelector(this.selectors.rebirthClick)
 
     this.rebirthCount.textContent = this.rebirth
     this.plusFunction()
@@ -46,7 +49,10 @@ class Combat {
   addCountFunction() {
     this.updateBtn.addEventListener('click', () => {
       if (this.balanceStorage >= 20) {
-        this.addPlus += 1 + (this.rebirth > 1 ? this.rebirth * 0.25 : 0)
+        this.addPlus += 1 + Math.round(this.rebirth > 1 ? this.rebirth * 0.25 : 0)
+
+
+
         this.balanceStorage -= 20
         this.updateCountText.textContent = this.addPlus
         localStorage.setItem('addPlus', this.addPlus)
@@ -59,14 +65,19 @@ class Combat {
 
   rebirthFunction() {
     this.rebirthBtn.addEventListener('click', () => {
-      if (this.addPlus >= 10 && this.balanceStorage >= this.rebirthPrice) {
+      // TET
+      if (this.addPlus >= this.rebirthAddPlusMust && this.balanceStorage >= this.rebirthPrice) {
         this.addPlus = 1
         this.balanceStorage = 0
         this.rebirth++
-        this.rebirthPrice = this.rebirthPrice * 2
+        // this.rebirthPrice = this.rebirthPrice * 2
+        this.rebirthPrice = this.rebirth >= 1 ? Math.round(this.rebirthPrice + this.rebirthPrice * 3) : this.rebirthPrice * 2
+        this.rebirthAddPlusMust = Math.round(this.rebirthAddPlusMust * 1.5)
+
         localStorage.setItem('balance', this.balanceStorage)
         localStorage.setItem('addPlus', this.addPlus)
         localStorage.setItem("rebirthPrice", this.rebirthPrice)
+        localStorage.setItem("rebirthAddPlusMust", this.rebirthAddPlusMust)
         localStorage.setItem('rebirth', this.rebirth)
         this.render()
         return
@@ -81,6 +92,8 @@ class Combat {
     this.balance.textContent = this.balanceStorage
     this.updateCountText.textContent = this.addPlus
     this.rebirthPriceTitle.textContent = this.rebirthPrice
+    this.rebirthCount.textContent = this.rebirth
+    this.rebirthClickEl.textContent = this.rebirthAddPlusMust
   }
 }
 
