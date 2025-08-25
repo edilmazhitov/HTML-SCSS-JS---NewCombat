cfd57885b01efd40ec5ddf45e18db60af41166d4
edilmazhitov <zusupovaerkinai@mail.com>
2025-08-25 20:07:47 +0600

    FINISH PROJECTS

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,14 @@
-
 class Combat {
   selectors = {
     balance: '[data-js-balance]',
     plus: '[data-js-game-button]',
-    update: '[data-js-add-new]',
-    updateCountPrice: '[data-js-add-new-price]',
+    update: '[ data-js-add-new]',
+    updateCountPrice: '[data-js-update-count-price]',
     updateCount: '[data-js-add-count]',
     rebirthCount: '[data-js-rebirth-count]',
     rebirthBtn: '[data-js-rebirth-btn]',
     rebirthPrice: '[data-js-rebirth-price]',
-    rebirthClick: '[data-js-click]'
+    rebirthClick: '[data-js-click]',
   }
 
   constructor() {
@@ -18,13 +17,16 @@ class Combat {
     this.addPlusPrice = Number(localStorage.getItem('addPlusPrice')) || 20
     this.rebirth = Number(localStorage.getItem('rebirth')) || 0
     this.rebirthPrice = Number(localStorage.getItem('rebirthPrice')) || 500
-    this.rebirthAddPlusMust = Number(localStorage.getItem("rebirthAddPlusMust")) || 10
+    this.rebirthAddPlusMust =
+      Number(localStorage.getItem('rebirthAddPlusMust')) || 10
 
     this.balance = document.querySelector(this.selectors.balance)
     this.plusBtn = document.querySelector(this.selectors.plus)
     this.updateBtn = document.querySelector(this.selectors.update)
     this.updateCountText = document.querySelector(this.selectors.updateCount)
-    this.updateCountPriceText = document.querySelector(this.selectors.updateCountPrice)
+    this.updateCountPriceText = document.querySelector(
+      this.selectors.updateCountPrice,
+    )
     this.rebirthPriceTitle = document.querySelector(this.selectors.rebirthPrice)
     this.rebirthCount = document.querySelector(this.selectors.rebirthCount)
     this.rebirthBtn = document.querySelector(this.selectors.rebirthBtn)
@@ -36,68 +38,69 @@ class Combat {
     this.render()
   }
 
-
-
-  addCountFunction() {
-    this.updateBtn.addEventListener('click', this.clickSystem)
-  }
-  rebirthFunction() {
-    this.rebirthBtn.addEventListener('click',this.rebirthSystem)
-  }
   plusFunction() {
-    this.plusBtn.addEventListener('click', this.plusSystem)
+    this.plusBtn.addEventListener('click', () => {
+      this.balanceStorage += this.addPlus
+      if (this.balance) {
+        this.balance.textContent = this.balanceStorage
+      }
+      localStorage.setItem('balance', this.balanceStorage)
+    })
     this.render()
   }
 
-  plusSystem = () => {
-    this.balanceStorage += this.addPlus
-    if (this.balance) {
-      this.balance.textContent = this.balanceStorage
-    }
-    localStorage.setItem('balance', this.balanceStorage)
+  addCountFunction() {
+    this.updateBtn.addEventListener('click', () => {
+      if (this.balanceStorage >= this.addPlusPrice) {
+        this.addPlus += 1 + Math.floor(this.rebirth * 0.5)
+
+        this.balanceStorage -= this.addPlusPrice
+
+        if (this.addPlus <= 10) {
+          this.addPlusPrice += 20 * 2
+        } else if (this.addPlus <= 20) {
+          this.addPlusPrice += 20 * 3
+        } else {
+          this.addPlusPrice += 20 * 4
+        }
+
+        localStorage.setItem('addPlusPrice', this.addPlusPrice)
+        localStorage.setItem('addPlus', this.addPlus)
+        localStorage.setItem('balance', this.balanceStorage)
+        this.updateCountText.textContent = this.addPlus
+        this.render()
+      }
+    })
   }
-  clickSystem = () => {
-    if (this.balanceStorage >= this.addPlusPrice) {
 
-      this.addPlus += 1 + Math.floor(this.rebirth * 0.5)
+  rebirthFunction() {
+    this.rebirthBtn.addEventListener('click', () => {
+      if (
+        this.addPlus >= this.rebirthAddPlusMust &&
+        this.balanceStorage >= this.rebirthPrice
+      ) {
+        this.addPlus = 1
+        this.balanceStorage = 0
+        this.rebirth++
+        this.rebirthPrice =
+          this.rebirth >= 1
+            ? Math.round(this.rebirthPrice + this.rebirthPrice * 3)
+            : this.rebirthPrice * 2
+        this.rebirthAddPlusMust = Math.round(this.rebirthAddPlusMust * 1.5)
+        this.addPlusPrice = 20
 
-      this.balanceStorage -= this.addPlusPrice
-
-      if (this.addPlus <= 10) {
-        this.addPlusPrice += 20 * 2
-      } else if (this.addPlus <= 20) {
-        this.addPlusPrice += 20 * 3
-      } else {
-        this.addPlusPrice += 20 * 4
+        localStorage.setItem('balance', this.balanceStorage)
+        localStorage.setItem('addPlus', this.addPlus)
+        localStorage.setItem('rebirthPrice', this.rebirthPrice)
+        localStorage.setItem('rebirthAddPlusMust', this.rebirthAddPlusMust)
+        localStorage.setItem('rebirth', this.rebirth)
+        localStorage.setItem('addPlusPrice', this.addPlusPrice)
+        this.render()
+        return
       }
 
-      localStorage.setItem('addPlusPrice', this.addPlusPrice)
-      localStorage.setItem('addPlus', this.addPlus)
-      localStorage.setItem('balance', this.balanceStorage)
-      this.updateCountText.textContent = this.addPlus
-      this.render()
-    }
-  }
-  rebirthSystem = () => {
-    if (this.addPlus >= this.rebirthAddPlusMust && this.balanceStorage >= this.rebirthPrice) {
-      this.addPlus = 1
-      this.balanceStorage = 0
-      this.rebirth++
-      this.rebirthPrice = this.rebirth >= 1 ? Math.round(this.rebirthPrice + this.rebirthPrice * 3) : this.rebirthPrice * 2
-      this.rebirthAddPlusMust = Math.round(this.rebirthAddPlusMust * 1.5)
-      this.addPlusPrice = 20;
-
-      localStorage.setItem('balance', this.balanceStorage)
-      localStorage.setItem('addPlus', this.addPlus)
-      localStorage.setItem("rebirthPrice", this.rebirthPrice)
-      localStorage.setItem("rebirthAddPlusMust", this.rebirthAddPlusMust)
-      localStorage.setItem('rebirth', this.rebirth)
-      localStorage.setItem("addPlusPrice", this.addPlusPrice)
-      this.render()
-      return
-    }
-
-    alert('Недостаточно средств для ребиртка')
+      alert('Недостаточно средств для ребиртка')
+    })
   }
 
   render() {
