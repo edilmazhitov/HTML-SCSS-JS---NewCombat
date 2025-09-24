806186b9c98630bec33092cec50defe21ff324d3
edilmazhitov <zusupovaerkinai@mail.com>
2025-09-24 15:43:30 +0600

    CLICKER POST

--- scripts/clickButton.js
+++ scripts/clickButton.js
@@ -10,17 +10,37 @@ class ClickButton {
       this.selectors.clickerButton,
     )
 
+    this.token = localStorage.getItem('token')
     this.plusSystems()
     this.#render()
   }
 
-  plusFunction = () => {
-    this.currentBalance = Number(localStorage.getItem('balance')) || 0
-    this.addPlus = Number(localStorage.getItem('addPlus')) || 1
-    this.currentBalance += this.addPlus
+  plusFunction = async () => {
+    try {
+      const amount = Number(localStorage.getItem('addPlus')) || 1
 
-    localStorage.setItem('balance', this.currentBalance)
-    this.balanceElement.textContent = this.currentBalance
+      const res = await fetch('http://localhost:5000/user/clickers', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.token}`,
+        },
+        body: JSON.stringify({ amount }),
+      })
+
+      if (!res.ok) {
+        const err = await res.json()
+        console.error('Ошибка сервера:', err.message)
+        return
+      }
+
+      const data = await res.json()
+
+      localStorage.setItem('balance', data.balance)
+      this.#render()
+    } catch (err) {
+      console.error('Ошибка запроса:', err)
+    }
   }
 
   plusSystems() {
@@ -28,7 +48,7 @@ class ClickButton {
   }
 
   #render() {
-    let currentBalance = Number(localStorage.getItem('balance')) || 0
+    const currentBalance = Number(localStorage.getItem('balance')) || 0
     this.balanceElement.textContent = currentBalance
   }
 }
